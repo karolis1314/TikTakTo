0fe34b3ba945689e4df8879e7b0d00c6afc7e000
Karolis <74104071+karolis1314@users.noreply.github.com>
2021-06-21 20:23:25 +0100

    Event created to get the id of the buttons which are clicked

--- app/src/main/java/com/example/tiktakto/MainActivity.kt
+++ app/src/main/java/com/example/tiktakto/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.tiktakto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun bClick(view: View) {
+
+        val bSelected = view as Button
+
+
     }
 }
