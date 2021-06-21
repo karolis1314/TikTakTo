acf012d2e82bf79a7a5a1d54dde06238795e0710
Karolis <74104071+karolis1314@users.noreply.github.com>
2021-06-21 20:26:23 +0100

    Cell selection was made

--- app/src/main/java/com/example/tiktakto/MainActivity.kt
+++ app/src/main/java/com/example/tiktakto/MainActivity.kt
@@ -15,6 +15,20 @@ class MainActivity : AppCompatActivity() {
 
         val bSelected = view as Button
 
+        var cellID = 0
+
+        when(bSelected.id){
+            R.id.b1 -> cellID = 1
+            R.id.b2 -> cellID = 2
+            R.id.b3 -> cellID = 3
+            R.id.b4 -> cellID = 4
+            R.id.b5 -> cellID = 5
+            R.id.b6 -> cellID = 6
+            R.id.b7 -> cellID = 7
+            R.id.b8 -> cellID = 8
+            R.id.b9 -> cellID = 9
+        }
+
 
     }
 }
